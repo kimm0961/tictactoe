b26459fe2f53490e862bd5e65ac550add5e487bf
kimmiesjensen <kimm0961@videndjurs.net>
2019-09-27 09:58:20 +0200

    Declaring a Winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,6 +11,9 @@ const Board = (props) => {
 
     const handleClick = (i) => {
         const sq = squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         sq[i] = xIsNext ? 'X' : 'O';
         setSquares (sq);
         setxIsNext(!xIsNext);
@@ -24,7 +27,36 @@ const Board = (props) => {
         );
     }
 
-    const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    // const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
 
     return ( 
 
