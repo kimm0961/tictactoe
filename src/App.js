599e743250899e7cbbca226c2c1ab6b0b71e0523
kimmiesjensen <kimm0961@videndjurs.net>
2019-09-27 09:01:27 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Tic Tac Toe</h1>
+      <Game></Game>
     </div>
   );
 }
---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Square = (props) => {
+    return ( 
+
+        <button className="square">
+            {/* TODO */}
+        </button>
+     );
+}
+ 
+export default Square;
